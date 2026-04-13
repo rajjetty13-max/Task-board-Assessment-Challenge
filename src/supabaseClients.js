d00dbe599ccbf0a8b9d9d42f01dfa7fcfff53b05
rajjetty13-max <rajjetty13@gmail.com>
2026-04-12 23:50:28 -0400

    Project set up: supabase client set up connection

---
+++ src/supabaseClients.js
@@ -0,0 +1,4 @@
+import {createClient} from '@supabase/supabase-js'
+const API_URL = import.meta.env.VITE_SUPABASE_API_URL
+const API_KEY = import.meta.env.VITE_SUPABASE_API_KEY
+export const supabase = createClient(API_URL, API_KEY)
